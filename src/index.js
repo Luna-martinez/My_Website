9b62bd9f19a8b0a68d98dbc6adc4d20741295763
Sujith <94170693+Luna-martinez@users.noreply.github.com>
2024-01-10 00:51:33 -0500

    My first changes to the App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import sujith_website from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <sujith_website />
   </React.StrictMode>
 );
 
